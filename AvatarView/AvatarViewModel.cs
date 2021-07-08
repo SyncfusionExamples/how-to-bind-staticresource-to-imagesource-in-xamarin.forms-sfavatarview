0aa39ceffca8fd71a1dc1e63577df7a2626a8d11
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-07-08 18:55:09 +0530

    Avatar view sample has been changed

---
+++ AvatarView/AvatarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AvatarView
+{
+    public class AvatarViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ImageSource avatarImage = "Avatar.png";
+        public ImageSource AvatarImage
+        {
+            get { return avatarImage; }
+            set { avatarImage = value; }
+        }
+    }
+}
